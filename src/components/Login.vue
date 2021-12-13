f6f16109246392688d767a05541da055365e2092
S4NT0R <santor3223@gmail.com>
2021-12-13 13:37:56 -0500

    Prueba

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Funciona</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:"LogIn",
+        data:{},
+        methods:{},
+        created: function(){}
+
+    }
+</script>
+
+<style>
+
+</style>
